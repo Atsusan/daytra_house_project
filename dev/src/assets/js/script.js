e6bf23ae1fdbc1986d9a05fda4aed89e6ec894da
Atsusan <as2k.sugimoto@gmail.com>
2024-05-14 00:47:48 +0900

    [update] voiceセクション構築　完了

--- dev/src/assets/js/script.js
+++ dev/src/assets/js/script.js
@@ -45,3 +45,54 @@ $('a[href^="#"]').click(function(event) {
     // デフォルトのクリック動作をキャンセル
     event.preventDefault();
 });
+
+// Swiper
+
+// JS 基本　初期化
+
+const mySwiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  loopAdditionalSlides: 1,
+  slidesPerView: 1,
+  nested: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    1320: {
+      slidesPerView: 5,
+      spaceBetween: 6,
+    },
+    // 768px以上の場合
+    768: {
+      slidesPerView: 4.5,
+      spaceBetween: 6,
+    }
+  },
+  
+  speed: 300, // スライドアニメーションのスピード（ミリ秒）
+
+  autoplay: { // 自動再生させる
+    delay: 3000, // 次のスライドに切り替わるまでの時間（ミリ秒）
+    disableOnInteraction: false, // ユーザーが操作しても自動再生を止めない
+    waitForTransition: false, // アニメーションの間も自動再生を止めない（最初のスライドの表示時間を揃えたいときに）
+  },
+ 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+ 
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
